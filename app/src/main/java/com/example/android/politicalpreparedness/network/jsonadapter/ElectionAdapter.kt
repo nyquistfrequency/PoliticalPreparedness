43ecff02590782e5565079e3d9936b495f619378
Adam <Adam.Natalli@bmw.de>
2023-08-31 15:16:22 +0200

    After Submission Feedback: Also adjust the ElectionAdapter.kt to also consider edge cases where no state is given (but only a district instead)

--- app/src/main/java/com/example/android/politicalpreparedness/network/jsonadapter/ElectionAdapter.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/network/jsonadapter/ElectionAdapter.kt
@@ -5,15 +5,28 @@ import com.squareup.moshi.FromJson
 import com.squareup.moshi.ToJson
 
 class ElectionAdapter {
+
+    //After Submission Feedback: Added a DistrictDelimiter to divisionFromJson to consider
+    //edge cases where state is null but district is given instead
+    //e.g. ocd-division/country:us/district:dc
     @FromJson
     fun divisionFromJson (ocdDivisionId: String): Division {
         val countryDelimiter = "country:"
         val stateDelimiter = "state:"
+        val districtDelimiter = "district:"
         val country = ocdDivisionId.substringAfter(countryDelimiter,"")
                 .substringBefore("/")
         val state = ocdDivisionId.substringAfter(stateDelimiter,"")
                 .substringBefore("/")
-        return Division(ocdDivisionId, country, state)
+
+        //Assumption: The district shall be used if no state is given, otherwise use the state
+        val district = ocdDivisionId.substringAfter(districtDelimiter,"")
+            .substringBefore("/")
+        return if (state == "") {
+            Division(ocdDivisionId,country,district)
+        } else {
+            Division(ocdDivisionId, country, state)
+        }
     }
 
     @ToJson
